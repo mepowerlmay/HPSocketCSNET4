8925c9400112a23e78872cf3382bb3410deee600
mepowerlmay <mepowerlmay@gmail.com>
2019-07-12 01:02:43 +0800

    first commit

---
+++ HPSocketCSNET4/IpAddress.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPSocketCSNET4
+{
+    public class EndPoint
+    {
+        public string Address { get; set; }
+        public ushort Port { get; set; }
+    }
+}
